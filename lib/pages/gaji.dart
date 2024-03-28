7cb6ab4a9f480a78e794ee292955de5da8566203
unknown <markoro1234@gmai.com>
2024-03-28 12:29:41 +0700

    initial commit

---
+++ lib/pages/gaji.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GajiPage extends StatelessWidget {
+  const GajiPage ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
