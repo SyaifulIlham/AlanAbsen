a10b249075b290a41342eb706feb219ff2d20ffa
gilangaliansyah <gilangaliansyah35@gmail.com>
2024-03-28 14:54:22 +0700

    pull

--- lib/pages/izin.dart
+++ lib/pages/izin.dart
@@ -5,6 +5,8 @@ class IzinPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Center(
+      child: Text('Halaman Home'),
+    );
   }
 }
